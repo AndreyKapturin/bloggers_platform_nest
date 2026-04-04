d0d9fc867c32ba6cf29b1f976963a7e5617c96a5
Andrey Kapturin <andruxa21097@yandex.ru>
2026-04-04 14:21:03 +0700

    feat: Testing route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserAccountsModule } from './modules/user-accounts/user-accounts.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BloggersPlatformModule } from './modules/bloggers-platform/bloggers-platform.module';
+import { TestingController } from './modules/testing/testing.controller';
+import { TestingModule } from './modules/testing/testing.module';
 const MONGO_URI = 'mongodb://0.0.0.0:27017/bloggers_platform';
 
 @Module({
-  imports: [MongooseModule.forRoot(MONGO_URI), UserAccountsModule, BloggersPlatformModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(MONGO_URI),
+    UserAccountsModule,
+    BloggersPlatformModule,
+    TestingModule,
+  ],
+  controllers: [TestingController],
 })
 export class AppModule {}
