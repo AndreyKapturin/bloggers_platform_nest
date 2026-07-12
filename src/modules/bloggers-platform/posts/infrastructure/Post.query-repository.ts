8a3583d9d960c6f546007346653687e04de38fb5
Andrey Kapturin <andruxa21097@yandex.ru>
2026-07-12 16:05:26 +0700

    feat: Posts query repository via type ORM

--- src/modules/bloggers-platform/posts/infrastructure/Post.query-repository.ts
+++ src/modules/bloggers-platform/posts/infrastructure/Post.query-repository.ts
@@ -17,6 +17,8 @@ import {
   DomainExceptionStatus,
 } from '../../../../core/exceptions/DomainException';
 import { PostReaction } from '../domain/PostReaction.entity';
+import { LikeStatus } from '../../dto/HttpLikeStatus.dto';
+import { SortDirection } from '../../../../core/dto/BaseQueryParams.dto';
 
 @Injectable()
 export class PostsQueryRepository {
@@ -107,80 +109,53 @@ export class PostsQueryRepository {
   ): Promise<PaginatedView<ViewPostDto>> {
     const { pageNumber, pageSize, skip, sortBy, sortDirection } = query;
 
-    const whereParams: (string | number | null)[] = [];
-    const conditions: string[] = [];
-
-    const fromPart = `FROM "posts" "p"`;
-
-    let wherePart = '';
-
-    if (filter.blogId) {
-      whereParams.push(filter.blogId);
-      conditions.push(`"blogId" = $${whereParams.length}`);
-    }
-
-    if (conditions.length > 0) {
-      wherePart += 'WHERE ' + conditions.join(' OR ');
-    }
-
-    const params = [...whereParams];
-
-    params.push(userId);
-    const joinPart = `
-    LEFT JOIN "blogs" "b" ON "b"."id" = "p"."blogId"
-    LEFT JOIN "postReactions" "pr" ON "pr"."postId" = "p"."id"
-    LEFT JOIN "postReactions" "ur" ON
-	    "ur"."postId" = "p"."id" AND
-	    "ur"."userId" = $${params.length}`;
-
-    params.push(pageSize);
-    const limitPart = `LIMIT $${params.length}`;
-
-    params.push(skip);
-    const offsetPart = `OFFSET $${params.length}`;
-
-    const getPostsSql = `
-      SELECT 
-        "p"."id",
-        "p"."title",
-        "p"."shortDescription",
-        "p"."content",
-        "p"."blogId",
-        "b"."name" AS "blogName",
-        "p"."createdAt",
-        COUNT(CASE WHEN "pr"."status" = 'Like' THEN 1 END)::integer AS "likesCount",
-        COUNT(CASE WHEN "pr"."status" = 'Dislike' THEN 1 END)::integer AS "dislikesCount",
-        CASE WHEN "ur"."status" IS NULL THEN 'None' ELSE "ur"."status" END AS "myStatus"
-      ${fromPart}
-      ${joinPart}
-      ${wherePart}
-      GROUP BY
-        "p"."id",
+    const queryBuilder = this.postEntityRepo
+      .createQueryBuilder('p')
+      .leftJoin('p.blog', 'b')
+      .leftJoin('p.reactions', 'pr')
+      .leftJoin('p.reactions', 'ur', 'ur.userId = :userId', { userId })
+      .select('p.id', 'id')
+      .addSelect('p.title', 'title')
+      .addSelect('p.shortDescription', 'shortDescription')
+      .addSelect('p.content', 'content')
+      .addSelect('p.blogId', 'blogId')
+      .addSelect('p.createdAt', 'createdAt')
+      .addSelect('b.name', 'blogName')
+      .addSelect(
+        `COUNT(CASE WHEN "pr"."status" = 'Like' THEN 1 END)::int`,
+        'likesCount',
+      )
+      .addSelect(
+        `COUNT(CASE WHEN "pr"."status" = 'Dislike' THEN 1 END)::int`,
+        'dislikesCount',
+      )
+      .addSelect(
+        `CASE WHEN "ur"."status" IS NULL THEN 'None' ELSE "ur"."status" END`,
+        'myStatus',
+      )
+      .groupBy(
+        `"p"."id",
         "p"."title",
         "p"."shortDescription",
         "p"."content",
         "p"."blogId",
         "b"."name",
         "p"."createdAt",
-        "ur"."status"
-      ORDER BY "${sortBy}" ${sortDirection}
-      ${limitPart}
-      ${offsetPart}`;
+        "ur"."status"`,
+      )
+      .orderBy(
+        `"${sortBy}"`,
+        sortDirection === SortDirection.Asc ? 'ASC' : 'DESC',
+      )
+      .offset(skip)
+      .limit(pageSize);
 
-    const posts = await this.dataSource.query<TExtendedPost[]>(
-      getPostsSql,
-      params,
-    );
+    if (filter.blogId) {
+      queryBuilder.where('p.blogId = :blogId', { blogId: filter.blogId })
+    }
 
-    const getTotalCountSql = `
-      SELECT
-        COUNT(*)::integer as "totalCount"
-      ${fromPart}
-      ${wherePart};`;
-
-    const [{ totalCount }] = await this.dataSource.query<
-      { totalCount: number }[]
-    >(getTotalCountSql, whereParams);
+    const posts = await queryBuilder.getRawMany<TExtendedPost>();
+    const totalCount = await queryBuilder.getCount();
 
     const postIds = posts.map(({ id }) => id);
     const newestLikes = await this._findNewestLikesForPosts(postIds);
@@ -209,18 +184,18 @@ export class PostsQueryRepository {
   private async _findNewestLikesForPost(
     postId: string,
   ): Promise<TViewNewestLike[]> {
-    return this.postReactionsEntityRepo.query<TViewNewestLike[]>(
-      `SELECT
-        "pr"."userId",
-        "u"."login",
-        "pr"."addedAt"
-      FROM "postReactions" "pr"
-      LEFT JOIN "users" "u" ON "u"."id" = "pr"."userId"
-      WHERE "pr"."postId" = $1 AND "pr"."status" = 'Like'
-      ORDER BY "pr"."addedAt" DESC
-      LIMIT 3;`,
-      [postId],
-    );
+    const newestLikes = await this.postReactionsEntityRepo
+      .createQueryBuilder('pr')
+      .leftJoin('users', 'u', 'u.id = pr.userId')
+      .select('pr.userId', 'userId')
+      .addSelect('u.login', 'login')
+      .addSelect('pr.addedAt', 'addedAt')
+      .where('pr.postId = :postId', { postId })
+      .andWhere('pr.status = :status', { status: LikeStatus.Like })
+      .orderBy('pr.addedAt', 'DESC')
+      .limit(3)
+      .getRawMany<TViewNewestLike>();
+    return newestLikes;
   }
 
   private async _findNewestLikesForPosts(
